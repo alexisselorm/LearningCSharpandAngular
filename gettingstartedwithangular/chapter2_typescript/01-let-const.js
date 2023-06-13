07585fb3864f8eae51bba38bb867f53d1c9dd4e2
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-06-13 23:44:42 +0200

    Initial commit

---
+++ gettingstartedwithangular/chapter2_typescript/01-let-const.js
@@ -0,0 +1,8 @@
+"use strict";
+let i = 1;
+for (let i = 1; i <= 5; i++) {
+    //do something
+}
+console.log(i);
+let start = 10;
+console.log(start);
---
+++ gettingstartedwithangular/chapter2_typescript/02-data-types.ts
@@ -0,0 +1,1 @@
+let taskCompleted: boolean = false;
