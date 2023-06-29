a9c0175ba856d87f859df2ff337424aa2f26567d
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-06-29 20:43:11 +0200

    Testing services

---
+++ gettingstartedwithangular/test-with-jasmine/src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor() {}
+
+  login() {}
+  logout() {}
+  isAuthenticated() {
+    const currentUser = localStorage.getItem('currentUser');
+    if (currentUser) {
+      return true;
+    }
+    return false;
+  }
+}
