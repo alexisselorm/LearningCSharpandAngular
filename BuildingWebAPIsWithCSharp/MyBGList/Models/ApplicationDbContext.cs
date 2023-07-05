d56609ea3021db944b67a0e65b77a7897e79a19b
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-07-05 23:33:10 +0200

    Added application db context and BoardGame model

---
+++ BuildingWebAPIsWithCSharp/MyBGList/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBGList.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //TODO: Custom code here
+        }
+
+    }
+}
