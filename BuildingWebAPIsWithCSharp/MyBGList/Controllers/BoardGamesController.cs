bbd4c579a0709a9778b600734f1111f93011c6fd
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-07-09 00:30:40 +0200

    Update and Delete records. CRUD complete

--- BuildingWebAPIsWithCSharp/MyBGList/Controllers/BoardGamesController.cs
+++ BuildingWebAPIsWithCSharp/MyBGList/Controllers/BoardGamesController.cs
@@ -46,5 +46,62 @@ namespace MyBGList.Controllers
         }
             };
         }
+
+
+        [HttpPatch(Name = "UpdateBoardGame")]
+        [ResponseCache(NoStore = true)]
+        public async Task<RestDTO<BoardGame?>> Patch(BoardGameDTO model)
+        {
+            var boardgame = await _context.BoardGames
+                .Where(b => b.Id == model.Id)
+                .FirstOrDefaultAsync();
+
+            if (boardgame != null)
+            {
+                if (!string.IsNullOrEmpty(model.Name))
+                    boardgame.Name = model.Name;
+
+                if (model.Year.HasValue && model.Year.Value > 0)
+                    boardgame.Year = model.Year.Value;
+                boardgame.LastModifiedDate = DateTime.Now;
+
+                _context.BoardGames.Update(boardgame);
+                await _context.SaveChangesAsync();
+
+
+            }
+            return new RestDTO<BoardGame?>()
+            {
+                Data = boardgame,
+                Links = new List<LinkDTO>
+                    {
+                        new LinkDTO(
+                            Url.Action(null,"BoardGames",model,Request.Scheme)!,"self","PATCH")
+            }
+            };
+        }
+
+
+        [HttpDelete(Name = "DeleteBoardGame")]
+        [ResponseCache(NoStore = true)]
+        public async Task<RestDTO<BoardGame?>> Delete(int id)
+        {
+            var boardgame = await _context.BoardGames
+                .Where(b => b.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (boardgame != null)
+                _context.BoardGames.Remove(boardgame);
+            await _context.SaveChangesAsync();
+
+            return new RestDTO<BoardGame?>()
+            {
+                Data = boardgame,
+                Links = new List<LinkDTO> {
+                    new LinkDTO(
+                Url.Action(null,"BoardGames",id,Request.Scheme)!,"self","DELETE")
+}
+            };
+        }
     }
 }
