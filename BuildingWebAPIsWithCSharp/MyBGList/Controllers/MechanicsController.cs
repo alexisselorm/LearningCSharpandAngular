f701230fa225f6ef33955ed9ab50d4aa19392792
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-07-12 23:37:34 +0200

    Mechanics controller and DTO added

---
+++ BuildingWebAPIsWithCSharp/MyBGList/Controllers/MechanicsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBGList.DTO;
+using MyBGList.Models;
+using System.Linq.Dynamic.Core;
+
+namespace MyBGList.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MechanicsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<MechanicsController> _logger;
+
+        public MechanicsController(ApplicationDbContext context, ILogger<MechanicsController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetDomains")]
+        [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
+        public async Task<RestDTO<Mechanic[]>> Get([FromQuery] RequestDTO<MechanicDTO> input)
+        {
+            var query = _context.Mechanics.AsQueryable();
+
+            if (!string.IsNullOrEmpty(input.FilterQuery))
+            {
+                query = query.Where(m => m.Name.Contains(input.FilterQuery));
+            }
+            query = query.
+                OrderBy($"{input.SortColumn} {input.SortOrder}").
+                Skip(input.PageIndex * input.PageSize).
+                Take(input.PageSize);
+
+            return new RestDTO<Mechanic[]>()
+            {
+                Data = await query.ToArrayAsync(),
+                PageIndex = input.PageIndex,
+                PageSize = input.PageSize,
+                RecordCount = await _context.Mechanics.CountAsync(),
+                Links = new List<LinkDTO>
+                {
+                    new LinkDTO(Url.Action(null,"Mechanics",new {input.PageIndex,input.PageSize},Request.Scheme)!,"self","GET")
+                }
+            };
+
+        }
+
+
+    }
+}
---
+++ BuildingWebAPIsWithCSharp/MyBGList/DTO/MechanicDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBGList.DTO
+{
+    public class MechanicDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+    }
+}
