ac2baeaf84c9e8e76336ad9733836d0e18b5852f
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-07-18 18:09:34 +0200

    Login Action and DTO added

--- BuildingWebAPIsWithCSharp/MyBGList/Controllers/AccountController.cs
+++ BuildingWebAPIsWithCSharp/MyBGList/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MyBGList.DTO;
 using MyBGList.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace MyBGList.Controllers
 {
@@ -77,9 +80,57 @@ namespace MyBGList.Controllers
 
         [HttpPost(Name = "Login")]
         [ResponseCache(CacheProfileName = "NoCache")]
-        public async Task<ActionResult> Login()
+        public async Task<ActionResult> Login(LoginDTO input)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = await _userManager.FindByNameAsync(input.UserName);
+                    if (user == null || !await _userManager.CheckPasswordAsync(user, input.Password))
+                    {
+                        throw new Exception("Invalid login attempt");
+                    }
+                    else
+                    {
+                        var signingCredentials = new SigningCredentials(
+                            new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration["JWT:SigningKey"])),
+                            SecurityAlgorithms.HmacSha256
+                            );
+
+
+                        var claims = new List<Claim>();
+                        claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+
+                        var jwtObject = new JwtSecurityToken(
+                            issuer: _configuration["JWT:Issuer"],
+                            audience: _configuration["JWT:Audience"],
+                            claims: claims,
+                            expires: DateTime.Now.AddSeconds(300),
+                            signingCredentials: signingCredentials);
+
+                        var jwtString = new JwtSecurityTokenHandler().WriteToken(jwtObject);
+                        return StatusCode(StatusCodes.Status200OK, jwtString);
+                    }
+                }
+                else
+                {
+                    var details = new ValidationProblemDetails(ModelState);
+                    details.Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+                    details.Status = StatusCodes.Status400BadRequest;
+                    return new BadRequestObjectResult(details);
+                }
+            }
+            catch (Exception e)
+            {
+                var exceptionDetails = new ProblemDetails();
+                exceptionDetails.Detail = e.Message;
+                exceptionDetails.Status = StatusCodes.Status401Unauthorized;
+                exceptionDetails.Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+                return StatusCode(
+                    StatusCodes.Status401Unauthorized, exceptionDetails);
+            }
+
         }
     }
 }
