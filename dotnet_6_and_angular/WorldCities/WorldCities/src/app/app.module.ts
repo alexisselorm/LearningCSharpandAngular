75bb8c96b64d9f40b4d5a3e47d5623970b4cf1de
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-08-11 12:22:01 +0200

    Project initial setup

---
+++ dotnet_6_and_angular/WorldCities/WorldCities/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    FetchDataComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule, AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
