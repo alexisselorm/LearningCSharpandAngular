96d3621da4c99b7d20537047082131ed4d35d821
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-08-11 12:41:08 +0200

    Added Angular Material

--- dotnet_6_and_angular/WorldCities/WorldCities/src/app/app.module.ts
+++ dotnet_6_and_angular/WorldCities/WorldCities/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
@@ -17,7 +19,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FetchDataComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule
+    BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
