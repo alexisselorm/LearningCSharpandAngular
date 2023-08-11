e63e439b73b8284c427fe55b4541cf670648c1b5
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-08-11 12:23:37 +0200

    Added angular material

--- dotnet_6_and_angular/WorldCities/WorldCities/src/app/app.module.ts
+++ dotnet_6_and_angular/WorldCities/WorldCities/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FetchDataComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, AppRoutingModule
+    BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
