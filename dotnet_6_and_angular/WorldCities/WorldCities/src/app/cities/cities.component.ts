045848433204404cd2f8648b2d20b96c789ad689
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-08-14 13:17:18 +0200

    Pagination, cool

--- dotnet_6_and_angular/WorldCities/WorldCities/src/app/cities/cities.component.ts
+++ dotnet_6_and_angular/WorldCities/WorldCities/src/app/cities/cities.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { environment } from '../../environments/environment';
 import { City } from './city';
 
@@ -9,14 +11,16 @@ import { City } from './city';
   styleUrls: ['./cities.component.scss']
 })
 export class CitiesComponent implements OnInit {
-  protected cities!: City[]
+  protected cities!: MatTableDataSource<City>;
   public displayedColumns: string[]=["id","name","lat","lon"]
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private http: HttpClient) { }
 
   getCities() {
     this.http.get<City[]>(environment.baseUrl + 'api/Cities').subscribe((result) => {
-      this.cities = result
+      this.cities = new MatTableDataSource<City>(result)
+      this.cities.paginator = this.paginator;
     }, error => console.log(error));
   }
 
