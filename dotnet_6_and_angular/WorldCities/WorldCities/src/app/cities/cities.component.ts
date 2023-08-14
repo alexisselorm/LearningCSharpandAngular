7cce75aef2cba4da0994b8391b0a584804190efb
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-08-14 14:26:51 +0200

    PageEvent pagination

--- dotnet_6_and_angular/WorldCities/WorldCities/src/app/cities/cities.component.ts
+++ dotnet_6_and_angular/WorldCities/WorldCities/src/app/cities/cities.component.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { environment } from '../../environments/environment';
 import { City } from './city';
 
@@ -15,17 +15,30 @@ export class CitiesComponent implements OnInit {
   public displayedColumns: string[]=["id","name","lat","lon"]
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+
   constructor(private http: HttpClient) { }
 
-  getCities() {
-    this.http.get<City[]>(environment.baseUrl + 'api/Cities').subscribe((result) => {
-      this.cities = new MatTableDataSource<City>(result)
-      this.cities.paginator = this.paginator;
+  getPageEvent() {
+    var pageEvent = new PageEvent();
+    pageEvent.pageIndex = 0;
+    pageEvent.pageSize = 10;
+    this.getCities(pageEvent);
+  }
+
+  getCities(event: PageEvent) {
+    let url = environment.baseUrl + 'api/Cities';
+    let params = new HttpParams().set("pageIndex", event.pageIndex.toString()).set("pageSize", event.pageSize.toString());
+    this.http.get<any>(url, {params}).subscribe((result) => {
+      this.paginator.length = result.totalCount;
+      this.paginator.pageIndex = result.pageIndex;
+      this.paginator.pageSize = result.pageSize;
+      this.cities = new MatTableDataSource<City>(result.data)
+      /*this.cities.paginator = this.paginator;*/
     }, error => console.log(error));
   }
 
 
   ngOnInit(): void {
-    this.getCities();
+    this.getPageEvent();
     }
 }
