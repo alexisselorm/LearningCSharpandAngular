3a0cf921a3057ea70fe658b9d67ad83314417ce5
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-11-22 00:21:58 +0200

    Cors

--- dotnet_6_and_angular/HealthCheck/HealthCheckAPI/Program.cs
+++ dotnet_6_and_angular/HealthCheck/HealthCheckAPI/Program.cs
@@ -13,13 +13,23 @@ builder.Services.AddCors(options =>
     });
 });
 builder.Services.AddHealthChecks()
-    .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com", 100))
-    .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
+    .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com", 200))
+    .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 200))
     .AddCheck("ICMP_03", new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name:"AngularPolicy", cfg =>
+    {
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+        cfg.WithOrigins(builder.Configuration["AllowedCORS"]);
+    });
+});
 
 var app = builder.Build();
 
@@ -40,5 +50,11 @@ app.UseHealthChecks(new PathString("/api/health"),
     new CustomHealthCheckOptions());
 
 app.MapControllers();
+
+//Minimal API heartbeat check
+app.MapMethods("/api/heartbeat",new []{"HEAD"}, () =>
+{
+    Results.Ok();
+});
 
 app.Run();
