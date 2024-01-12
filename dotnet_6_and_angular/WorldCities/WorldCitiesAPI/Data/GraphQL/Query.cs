2e1e27e464c1b4bb74ef013c0b96ef4953c11b4d
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-01-12 11:06:31 +0200

    Add apiresulttype of implementation to asp net graphql query endpoint

--- dotnet_6_and_angular/WorldCities/WorldCitiesAPI/Data/GraphQL/Query.cs
+++ dotnet_6_and_angular/WorldCities/WorldCitiesAPI/Data/GraphQL/Query.cs
@@ -1,4 +1,6 @@
-﻿using WorldCitiesAPI.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCitiesAPI.Data.Models;
+using WorldCitiesAPI.Data.ResponseTypes;
 
 namespace WorldCitiesAPI.Data.GraphQL
 {
@@ -18,5 +20,30 @@ namespace WorldCitiesAPI.Data.GraphQL
         [UseFiltering]
         [UseSorting]
         public IQueryable<Country> GetCountries([Service] ApplicationDbContext context) => context.Countries;
+
+
+        /// <summary 
+        /// Get all Cities(APIResult and DTO Support)
+        /// </summary>
+        [Serial]
+        public async Task<ApiResult<CityDTO>> GetCitiesApiResult([Service] ApplicationDbContext context,
+            int pageIndex = 0,
+            int pageSize = 10,
+            string? sortColumn = null,
+            string? sortOrder = null,
+            string? filterColumn = null,
+            string? filterQuery = null)
+        {
+            return await ApiResult<CityDTO>.CreateAsync(context.Cities.AsNoTracking().Select(c => new CityDTO
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Lat = c.Lat,
+                Lon = c.Lon,
+                CountryId = c.Country!.Id,
+                CountryName = c.Country.Name
+            }), pageIndex, pageSize, sortColumn, sortOrder, filterColumn, filterQuery);
+        }
+
     }
 }
