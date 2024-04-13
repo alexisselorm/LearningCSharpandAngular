0dc082b4f041f0bec363e709380972538ecacfb1
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-04-13 03:33:16 +0200

    SignalR health hub service implementation

---
+++ dotnet_6_and_angular/HealthCheck/HealthCheck/src/app/health-check/health-check.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class HealthCheckService {
+  private hubConnection!: signalR.HubConnection;
+  private _result: Subject<Result> = new Subject<Result>();
+  public result = this._result.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  public startConnection() {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .configureLogging(signalR.LogLevel.Information)
+      .withUrl(environment.baseUrl + 'api/health-hub', {
+        withCredentials: false,
+      })
+      .build();
+
+    console.log('Starting connection...');
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch((err: any) => console.log(err));
+
+    this.updateData();
+  }
+
+  public updateData() {
+    console.log('Fetching data...');
+    this.http
+      .get<Result>(environment.baseUrl + 'api/health')
+      .subscribe((res) => {
+        this._result.next(res);
+        console.log(res);
+      });
+  }
+}
+
+export interface Result {
+  checks: Check[];
+  totalStatus: string;
+  totalResponseTime: number;
+}
+
+interface Check {
+  name: string;
+  responseTime: number;
+  status: string;
+  description: string;
+}
